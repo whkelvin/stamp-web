61175554640fac76b1e8f9178f27ddcd2c77fa43
whkelvin <whkelvin.hsu@gmail.com>
2023-04-18 22:42:35 -0300

    added in a theme test page

---
+++ src/components/translate.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let text: string;
+</script>
+
+<div>
+	{{ text }}
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,24 @@ export default {
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#80BBAD',
+          secondary: '#81a1c1',
+          accent: '#D08770',
+          neutral: '#2E3440',
+          'neutral-content': '#A6ADBB',
+          'base-100': '#3b4252',
+          'base-content': '#A6ADBB',
+          info: '#5E81ac',
+          success: '#a3be8c',
+          warning: '#EBCB8B',
+          error: '#BF616A'
+        }
+      }
+    ]
+  }
 };
